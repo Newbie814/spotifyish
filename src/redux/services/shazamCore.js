65ac4d0814011b7c5184f1d83bf3b4ae17abebd2
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-09-26 20:26:24 -0400

    implementing shazam api calls

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': 'f9df83b4b1mshc837a547be5fc50p13e477jsnc5fa5c5f33d1',
+//     'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
+//   },
+// };
+
+// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        'f9df83b4b1mshc837a547be5fc50p13e477jsnc5fa5c5f33d1'
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
