1869ef79f9d1ed889c13c192f69d6afab6360d71
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-10-01 23:16:35 -0400

    implemented play/pause aesthetics

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,13 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+/* eslint-disable no-confusing-arrow */
+/* eslint-disable jsx-quotes */
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle size={35} className='text-gray-300' onClick={handlePause} />
+  ) : (
+    <FaPlayCircle size={35} className='text-gray-300' onClick={handlePlay} />
+  );
 
 export default PlayPause;
