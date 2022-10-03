d549af8ecda28ed71a242053a0eb6cc4de6999dc
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-10-03 14:13:04 -0400

    building related songs functionality - api calls, play/pause, rendering, etc

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -5,19 +5,34 @@ import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
+import {
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} from '../redux/services/shazamCore';
 
 const SongDetails = () => {
   const dispatch = useDispatch();
   const { songid } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
 
+  const {
+    data,
+    isFetching: isFetchinRelatedSongs,
+    error,
+  } = useGetSongRelatedQuery({ songid });
   const { data: songData, isFetching: isFetchingSongDetails } =
     useGetSongDetailsQuery({ songid });
 
+  if (isFetchingSongDetails && isFetchinRelatedSongs)
+    return <Loader title='Searching song details' />;
+
+  console.log(songData);
+
+  if (error) return <Error />;
+
   return (
     <div className='flex flex-col'>
-      {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+      <DetailsHeader artistId='' songData={songData} />
 
       <div className='mb-10'>
         <h2 className='text-white text-3xl font-bold'>Lyrics:</h2>
@@ -39,6 +54,14 @@ const SongDetails = () => {
           )}
         </div>
       </div>
+      {/* <RelatedSongs
+        data={data}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      /> */}
     </div>
   );
 };
