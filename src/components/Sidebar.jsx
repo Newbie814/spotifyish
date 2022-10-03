df8c5bef2b6c3dc29d2b629bf8a5daf7a17706a6
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-10-02 20:13:14 -0400

    added top charts and top artists widgets and functionality

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,13 @@ import {
 import { RiCloseLine } from 'react-icons/ri';
 
 import { logo } from '../assets';
-import { links } from '../assets/constants';
+
+const links = [
+  { name: 'Discover', to: '/', icon: HiOutlineHome },
+  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
+  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
+  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+];
 
 const NavLinks = ({ handleClick }) => (
   <div className='mt-10'>
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // const options = {
