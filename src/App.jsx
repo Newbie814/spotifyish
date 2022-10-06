d4652621171964bd52f67b206bb78bf45a3c817c
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-10-06 17:57:29 -0400

    completely changing location of the searchbar component to see if it renders anywhere

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,8 @@ const App = () => {
 
   return (
     <div className='relative flex'>
+      <Sidebar />
       <div className='flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]'>
-        <Searchbar />
-        <Sidebar />
-
         <div className='px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse'>
           <div className='flex-1 h-fit pb-40'>
             <Routes>
@@ -36,6 +34,7 @@ const App = () => {
           </div>
           <div className='xl:sticky relative top-0 h-fit'>
             <TopPlay />
+            <Searchbar />
           </div>
         </div>
       </div>
