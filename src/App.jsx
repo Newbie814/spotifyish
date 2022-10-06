d4b1d0b1cd268cdcac884e366d2889fbdc4c5ea4
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-10-06 18:12:30 -0400

    wrapping searchbar in it's own div

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,9 @@ const App = () => {
     <div className='relative flex'>
       <Sidebar />
       <div className='flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]'>
+        <div>
+          <Searchbar />
+        </div>
         <div className='px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse'>
           <div className='flex-1 h-fit pb-40'>
             <Routes>
@@ -34,7 +37,6 @@ const App = () => {
           </div>
           <div className='xl:sticky relative top-0 h-fit'>
             <TopPlay />
-            <Searchbar />
           </div>
         </div>
       </div>
